73d4becd672e48a417dd831c77c9d6e708499461
Toju Ofeoritse Nathaniel <tojuofe@gmail.com>
2020-04-23 19:29:45 +0100

    Changed the sidenav to left

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,12 @@
 let btnControl = document.querySelectorAll('.btnclick');
 let formContent = document.querySelectorAll('.staff-form');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+const toggle = document.getElementById('toggle');
 
 for (let i = 0; i < btnControl.length; i++) {
-  btnControl[i].addEventListener('click', function() {
+  btnControl[i].addEventListener('click', function () {
     for (let i = 0; i < formContent.length; i++) {
       formContent[i].className = 'staff-form';
     }
@@ -14,3 +18,19 @@ for (let i = 0; i < btnControl.length; i++) {
     this.className = 'btnclick active';
   });
 }
+
+// Toggle nav
+toggle.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav')
+);
+
+// Show
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide Modal on outside Click
+// window.addEventListener('click', (e) =>
+//   e.target == modal ? modal.classList.remove('show-modal') : false
+// );
